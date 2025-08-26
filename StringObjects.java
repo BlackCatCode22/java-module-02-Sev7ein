c2d72f40bb67ece8d46d4044214a70c90346c4c5
Sev7ein <0914473@my.scccd.edu>
2025-08-26 11:19:57 -0700

    String Objects For Java

---
+++ StringObjects.java
@@ -0,0 +1,23 @@
+//8/26/25
+//s.n
+
+public class StringObjects {
+    public static void main(String[] args) {
+    String phrase = "Best Artists Of 2025";
+        System.out.println( phrase.substring(0,phrase.indexOf(" ")));
+        System.out.println( phrase.toUpperCase());
+        System.out.println( phrase.toLowerCase());
+        System.out.print(IndexOf());
+        System.out.println( phrase.substring(0, 12));
+        System.out.println( phrase.contains("Artists"));
+
+
+    }
+
+    private static boolean IndexOf() {
+        return false;
+    }
+
+
+}
+
