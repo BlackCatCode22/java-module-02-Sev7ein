bf86b21b9aa3967bbe6a81ddef1c33c61f464a50
Sev7ein <0914473@my.scccd.edu>
2025-08-26 11:43:39 -0700

    Next commit

---
+++ ForLoopsPractice.java
@@ -0,0 +1,18 @@
+// 08/26/25
+// s.n
+
+
+public class ForLoopsPractice {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i <= 1000000) {
+            System.out.println(i);
+            i++;
+        }
+        // A Loop must have a loop control variable.
+        // The loop control variable must be initialized, checked, and changed.
+        for(i = 1; i <= 100000000; i++) {
+
+        }
+    }
+}
