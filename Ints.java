e820e6319a161f5654a0d735487b6c0e345e0a92
Sev7ein <0914473@my.scccd.edu>
2025-09-02 12:00:46 -0700

    Added 2 from 9/2/25

---
+++ Ints.java
@@ -0,0 +1,17 @@
+// 09/02/25
+// s.n
+
+public class Ints {
+    public static void main(String[] args) {
+    int x = 5;
+    int doppleGanger = 10;
+
+    int twin = sumOfInts(x, doppleGanger);
+        }
+        static int sumOfInts(int x, int doppleGanger) {
+            int twin = x + doppleGanger;
+            System.out.println(twin);
+            return twin;
+
+    }
+}
